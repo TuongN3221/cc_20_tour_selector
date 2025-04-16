b42017fc7911ce75d294fd96461b69b5763228d6
Tuong Nguyen <yolotube21@yahoo.com>
2025-04-16 12:59:10 -0400

    Implement TourCard component with dynamic display and removal

---
+++ src/components/TourCard.jsx
@@ -0,0 +1,29 @@
+// Displays tour details: name, info, image, price
+//Include a "Not Interested" button to remove the tour from the list
+import React, {useState} from "react";
+
+const TourCard = ({ id, name, info, price, image, onRemove}) => {
+    const [readMore, setReadMore] = useState(false);
+
+    return (
+        <article className = "tour-card">
+            <h3>{ name }</h3>
+            <h5>{ info } </h5>
+            <img src={ image } alt={ name } className="tour-image" />
+            <h4> Price: ${ price }</h4>
+            <p>
+                {/* Shows full description if readMore is true. Otherwise, slice */}
+                { readMore ? info : `${ info.substring(0, 200) }...` }
+                <button onClick={() => setReadMore(!readMore)}>
+                    {/* Toggles betweeen Read More and Show Less*/}
+                    { readMore ? "Show Less" : "Read More" }
+                </button>
+            </p>
+            <button className ="remove-btn" onClick={() => onRemove(id)}>
+                Not Interested
+            </button>
+        </article>
+    )
+
+}
+export default TourCard
